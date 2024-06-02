3834efbe1a7006f2b0655dec2351718394c593f1
09btmhabib <mazhar.habib250@gmail.com>
2024-06-02 05:44:03 +0500

    init

--- src/main/java/com/assignment/userpostservice/auth/controller/AuthController.java
+++ src/main/java/com/assignment/userpostservice/auth/controller/AuthController.java
@@ -6,7 +6,6 @@ import com.assignment.userpostservice.auth.dto.LoginResponseDto;
 import com.assignment.userpostservice.auth.service.AuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -22,10 +21,15 @@ public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-    @Autowired
-    private AuthService authService;
-    @Autowired
-    private AuthenticationManager authenticationManager;
+
+    private final AuthService authService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager) {
+        this.authService = authService;
+        this.authenticationManager = authenticationManager;
+    }
 
     @PostMapping("/login")
     public LoginResponseDto login(@RequestBody LoginRequestDto userLogin) {
